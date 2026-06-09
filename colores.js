5dbc0f43a92ee9b94130ccdbae14897413b78ba3
menorcatic <menorcatic@hotmail.com>
2026-06-09 16:39:30 +0200

    Refactor color configuration logic for sensors

--- colores.js
+++ colores.js
@@ -10,27 +10,27 @@ function obtenerColorRelleno(idSensor, valor) {
     if (isNaN(num)) return configuracion;
     
     if (idSensor === 'PotPlacas') {
-        if (num === 0) return configuracion; // De noche, por defecto
-        if (num < 400) configuracion.fondo = 'rgb(255, 0, 0)'; // Rojo
-        else if (num < 1000) { configuracion.fondo = 'rgb(255, 165, 0)'; configuracion.texto = '#000000'; } // Naranja -> Texto Negro
-        else if (num < 2000) { configuracion.fondo = 'rgb(255, 255, 0)'; configuracion.texto = '#000000'; } // Amarillo -> Texto Negro
-        else configuracion.fondo = 'rgb(0, 255, 0)'; // Verde
+        if (num === 0) return configuracion; 
+        if (num < 400) { configuracion.fondo = 'rgb(255, 0, 0)'; } 
+        else if (num < 1000) { configuracion.fondo = 'rgb(255, 165, 0)'; configuracion.texto = '#000000'; } 
+        else if (num < 2000) { configuracion.fondo = 'rgb(255, 255, 0)'; configuracion.texto = '#000000'; } 
+        else { configuracion.fondo = 'rgb(0, 255, 0)'; }
     }
 
     else if (idSensor === 'PotCons') {
         if (num === 0) return configuracion;
-        if (num < 300) configuracion.fondo = 'rgb(0, 255, 0)'; // Verde
-        else if (num < 500) { configuracion.fondo = 'rgb(255, 255, 0)'; configuracion.texto = '#000000'; } // Amarillo -> Texto Negro
-        else if (num < 800) { configuracion.fondo = 'rgb(255, 165, 0)'; configuracion.texto = '#000000'; } // Naranja -> Texto Negro
-        else configuracion.fondo = 'rgb(255, 0, 0)'; // Rojo
+        if (num < 300) { configuracion.fondo = 'rgb(0, 255, 0)'; } 
+        else if (num < 500) { configuracion.fondo = 'rgb(255, 255, 0)'; configuracion.texto = '#000000'; } 
+        else if (num < 800) { configuracion.fondo = 'rgb(255, 165, 0)'; configuracion.texto = '#000000'; } 
+        else { configuracion.fondo = 'rgb(255, 0, 0)'; }
     }
     
     else if (idSensor === 'VBat') {
         if (num === 0) return configuracion;
-        if (num < 24) configuracion.fondo = 'rgb(255, 0, 0)'; // Rojo
-        else if (num < 25) { configuracion.fondo = 'rgb(255, 165, 0)'; configuracion.texto = '#000000'; } // Naranja -> Texto Negro
-        else if (num < 26) { configuracion.fondo = 'rgb(255, 255, 0)'; configuracion.texto = '#000000'; } // Amarillo -> Texto Negro
-        else configuracion.fondo = 'rgb(0, 255, 0)'; // Verde
+        if (num < 24) { configuracion.fondo = 'rgb(255, 0, 0)'; } 
+        else if (num < 25) { configuracion.fondo = 'rgb(255, 165, 0)'; configuracion.texto = '#000000'; } 
+        else if (num < 26) { configuracion.fondo = 'rgb(255, 255, 0)'; configuracion.texto = '#000000'; } 
+        else { configuracion.fondo = 'rgb(0, 255, 0)'; }
     }
     
     return configuracion;
