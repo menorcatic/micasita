6743e813b51c621900bcf791b1fb89ad33ad5c0e
menorcatic <menorcatic@hotmail.com>
2026-06-10 10:39:13 +0200

    Fix condition for background color in PotPlaca

--- colores.js
+++ colores.js
@@ -34,7 +34,7 @@ function obtenerColorRelleno(idSensor, valor) {
     }
     else if (idSensor === 'PotPlaca') {
         if (num === 0) return configuracion; 
-        if (num < 400) { configuracion.fondo = 'rgb(255, 0, 0)'; } 
+        if (PotPlaca < PotCons) { configuracion.fondo = 'rgb(255, 0, 0)'; } 
         else if (num < 1000) { configuracion.fondo = 'rgb(255, 165, 0)'; configuracion.texto = '#000000'; } 
         else if (num < 2000) { configuracion.fondo = 'rgb(255, 255, 0)'; configuracion.texto = '#000000'; } 
         else { configuracion.fondo = 'rgb(0, 255, 0)'; }
